8cf2ab8edfc407139b8791d7d241d06e13f50cef
SaketSinha2005 <saket.sinha.dev@gmail.com>
2026-09-22 06:17:11 +0530

    Added add function

---
+++ bin/index.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+const fs = require("node:fs");
+var {argv} = require('node:process');
+
+argv = Object.entries(argv).slice(2).map(entry => entry[1]);
+// console.log(arg);
+// process_file(argv[0], undefined, argv[1]);
+process_file(argv[0], argv[1], argv[2]);
+
+function get_id(data) {
+    if (data.length === 0) return 1;
+    return data[data.length - 1].id + 1;
+}
+
+function process_file(val, id, msg){
+    const filePath = "D:\\backend_dev\\data.json";
+
+    if(!fs.existsSync(filePath)){
+        fs.writeFileSync(filePath, "[]");
+    }
+
+    let filedata = fs.readFileSync(filePath, 'utf8');
+    let data = JSON.parse(filedata);
+    const datetime = new Date(); 
+
+    if(val === `add`){
+        data.push({
+            id: get_id(data),
+            description: msg,
+            status: "todo",
+            createdAt: datetime,
+            updatedAt: datetime
+        });
+
+        fs.writeFile(filePath, JSON.stringify(data, null, 2), (err) => {
+            if(err) throw err;
+            else console.log(`Entry added with id: ${get_id(data)-1}`);
+        });
+    }
+    else if(val === `update`){
+        let data
+        try{ 
+            let data2 = data[id-1];
+            console.log(data2);
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+}
